20219435bacd2290ba35dec9bd74c0b744464ebd
Riccardo Orlando <orlandoricc@gmail.com>
2023-02-08 18:37:49 +0100

    Update code

--- src/models/model.py
+++ src/models/model.py
@@ -9,8 +9,9 @@ import transformers as tr
 class BaseModel(torch.nn.Module):
     def __init__(
         self,
-        question_encoder: Union[str, tr.PreTrainedModel] = "bert-base-cased",
-        context_encoder: Union[str, tr.PreTrainedModel] = "bert-base-cased",
+        language_model: Union[str, tr.PreTrainedModel] = "bert-base-cased",
+        question_encoder: Optional[Union[str, tr.PreTrainedModel]] = None,
+        context_encoder: Optional[Union[str, tr.PreTrainedModel]] = None,
         labels: Labels = None,
         *args,
         **kwargs,
@@ -18,17 +19,22 @@ class BaseModel(torch.nn.Module):
         super().__init__()
         if labels is not None:
             self.labels = labels
+        
+        if not question_encoder:
+            question_encoder = language_model
         if isinstance(question_encoder, str):
             self.question_encoder = tr.AutoModel.from_pretrained(question_encoder)
         else:
             self.question_encoder = question_encoder
+        if not context_encoder:
+            context_encoder = language_model
         if isinstance(context_encoder, str):
             self.context_encoder = tr.AutoModel.from_pretrained(context_encoder)
         else:
             self.context_encoder = context_encoder
 
-    @staticmethod
     def encode(
+        self,
         encoder: tr.PreTrainedModel,
         input_ids: torch.Tensor,
         attention_mask: torch.Tensor,
@@ -39,7 +45,18 @@ class BaseModel(torch.nn.Module):
         model_kwargs = {"input_ids": input_ids, "attention_mask": attention_mask}
         if token_type_ids is not None:
             model_kwargs["token_type_ids"] = token_type_ids
-        return encoder(**model_kwargs).pooler_output
+        return self.mean_pooling(encoder(**model_kwargs), attention_mask)
+
+    def mean_pooling(self, model_output, attention_mask):
+        token_embeddings = model_output[
+            0
+        ]  # First element of model_output contains all token embeddings
+        input_mask_expanded = (
+            attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        )
+        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(
+            input_mask_expanded.sum(1), min=1e-9
+        )
 
     def forward(
         self,
