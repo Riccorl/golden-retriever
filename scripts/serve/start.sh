44d35bd1eabcc122a8475341612bab67cd72c172
Riccardo Orlando <orlandoricc@gmail.com>
2023-04-12 13:47:36 +0000

    Fix dockerfiles

--- scripts/serve/start.sh
+++ scripts/serve/start.sh
@@ -1,26 +1,28 @@
 #! /usr/bin/env sh
 set -e
 
-if [ -f /app/src/main.py ]; then
-    DEFAULT_MODULE_NAME=app.main
-elif [ -f /app/main.py ]; then
-    DEFAULT_MODULE_NAME=main
-fi
+# if [ -f /app/src/main.py ]; then
+#     DEFAULT_MODULE_NAME=app.main
+# elif [ -f /app/main.py ]; then
+#     DEFAULT_MODULE_NAME=main
+# fi
+DEFAULT_MODULE_NAME=golden_retriever.serve.app:app
 MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
 VARIABLE_NAME=${VARIABLE_NAME:-app}
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
 
-if [ -f /app/gunicorn_conf.py ]; then
-    DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
-elif [ -f /app/app/gunicorn_conf.py ]; then
-    DEFAULT_GUNICORN_CONF=/app/app/gunicorn_conf.py
-else
-    DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
-fi
+# if [ -f /app/gunicorn_conf.py ]; then
+#     DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
+# elif [ -f /app/app/gunicorn_conf.py ]; then
+#     DEFAULT_GUNICORN_CONF=/app/app/gunicorn_conf.py
+# else
+#     DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
+# fi
+DEFAULT_GUNICORN_CONF=/serve/gunicorn_conf.py
 export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
 
 # If there's a prestart.sh script in the /app directory, run it before starting
-PRE_START_PATH=/app/prestart.sh
+PRE_START_PATH=/serve/prestart.sh
 if [ -f $PRE_START_PATH ] ; then
     . "$PRE_START_PATH"
 else
